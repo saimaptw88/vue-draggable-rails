ceb84221d310783f45b1f52f754b6bf00c6997a1
斎藤正大 <saima.ptw88@gmail.com>
2021-06-02 12:21:18 +0900

    vuedraggable motion check

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Item.create(name: "apple", categoryNo: 1, index: 0)
+Item.create(name: "google", categoryNo: 1, index: 1)
+Item.create(name: "amazon", categoryNo: 1, index: 2)
